b3d84143bfc70feb7dd06af3bc01c5d4272daf2f
Kamal Chauhan <kamalchauhan0037@gmail.com>
2026-06-16 03:56:50 +0530

    fix: terraform variables syntax error

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,60 +1,28 @@
-variable "environment" {
-  description = "Deployment environment (dev or prod)"
-  type        = string
-  validation {
-    condition     = contains(["dev", "prod"], var.environment)
-    error_message = "Environment must be 'dev' or 'prod'."
-  }
+variable "resource_group_name" {
+  type = string
 }
 
 variable "location" {
-  description = "Azure region"
-  type        = string
-  default     = "East US"
-}
-
-variable "resource_group_name" {
-  description = "Name of the resource group"
-  type        = string
+  type = string
 }
 
 variable "app_gateway_name" {
-  description = "Name of the Application Gateway"
-  type        = string
+  type = string
 }
 
-variable "container_app_name" {
-  description = "Name of the Container App"
-  type        = string
+variable "environment" {
+  type = string
 }
 
-variable "container_image" {
-  description = "Docker image for the container app"
-  type        = string
+variable "subnet_id" {
+  type = string
 }
 
-variable "container_app_env_name" {
-  description = "Name of the Container App Environment"
-  type        = string
-}
-
-variable "vnet_name" {
-  description = "Name of the Virtual Network"
-  type        = string
-}
-
-variable "appgw_subnet_name" {
-  description = "Subnet name for Application Gateway"
-  type        = string
-}
-
-variable "container_subnet_name" {
-  description = "Subnet name for Container App Environment"
-  type        = string
+variable "backend_fqdn" {
+  type = string
 }
 
 variable "tags" {
-  description = "Tags to apply to all resources"
-  type        = map(string)
-  default     = {}
+  type    = map(string)
+  default = {}
 }
