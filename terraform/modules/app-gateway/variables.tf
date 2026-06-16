de3bc065b1bfea6ee9bf17b8e443ad0c7236d0fb
Kamal Chauhan <kamalchauhan0037@gmail.com>
2026-06-17 02:19:50 +0530

    fix: remove semicolons from variables.tf

--- terraform/modules/app-gateway/variables.tf
+++ terraform/modules/app-gateway/variables.tf
@@ -1,7 +1,28 @@
-variable "resource_group_name" { type = string }
-variable "location"            { type = string }
-variable "app_gateway_name"    { type = string }
-variable "environment"         { type = string }
-variable "subnet_id"           { type = string }
-variable "backend_fqdn"        { type = string }
-variable "tags"                { type = map(string); default = {} }
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "app_gateway_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "backend_fqdn" {
+  type = string
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
--- terraform/modules/container-app/variables.tf
+++ terraform/modules/container-app/variables.tf
@@ -1,8 +1,32 @@
-variable "resource_group_name" { type = string }
-variable "location"            { type = string }
-variable "container_app_env_name" { type = string }
-variable "container_app_name"  { type = string }
-variable "container_image"     { type = string }
-variable "environment"         { type = string }
-variable "subnet_id"           { type = string }
-variable "tags"                { type = map(string); default = {} }
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "container_app_env_name" {
+  type = string
+}
+
+variable "container_app_name" {
+  type = string
+}
+
+variable "container_image" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
